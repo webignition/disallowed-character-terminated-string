c6940cfd6929984c77740e713fa13979d3cf0f5e
Jon Cram <webignition@gmail.com>
2019-12-20 14:56:01 +0000

    Support PHP 7.2+ only (#6)

--- src/DisallowedCharacterTerminatedString.php
+++ src/DisallowedCharacterTerminatedString.php
@@ -1,77 +1,60 @@
 <?php
+
+declare(strict_types=1);
+
 namespace webignition\DisallowedCharacterTerminatedString;
 
 /**
  * A string terminated by the presence of a disallowed character
- *  
  */
 class DisallowedCharacterTerminatedString {
-    
+
     /**
      * Collection of characters not allowed
-     * 
+     *
      * @var array
      */
-    private $disallowedValueCharacterCodes = array();   
-    
+    private $disallowedValueCharacterCodes = [];
+
     /**
-     *
      * @var string
      */
     private $value = '';
-    
-    
-    /**
-     * Specify another character code whose occurence terminates the string
-     * 
-     * @param int $characterCode 
-     */
-    public function addDisallowedCharacterCode($characterCode) {
+
+    public function addDisallowedCharacterCode(int $characterCode): void
+    {
         if (!in_array($characterCode, $this->disallowedValueCharacterCodes)) {
             $this->disallowedValueCharacterCodes[] = $characterCode;
         }
     }
 
-    /**
-     * Return object to default state
-     *  
-     */
-    public function reset() {
+    public function reset(): void
+    {
         $this->value = '';
         $this->disallowedValueCharacterCodes = array();
     }
-    
-    /**
-     *
-     * @return string
-     */
-    public function get() {
+
+    public function get(): string
+    {
         return $this->value;
     }
-    
-    
-    /**
-     * 
-     * @param string $value 
-     */
-    public function set($value) {
-        $valueCharacters = str_split(trim((string)$value));        
+
+    public function set(string $value): void
+    {
+        $valueCharacters = str_split(trim((string)$value));
         $this->value = '';
-        
+
         foreach ($valueCharacters as $valueCharacter) {
             if (in_array(ord($valueCharacter), $this->disallowedValueCharacterCodes)) {
                 return;
             }
-            
+
             $this->value .= $valueCharacter;
         }
     }
-    
-    /**
-     *
-     * @return string
-     */
-    public function __toString() {
+
+    public function __toString(): string
+    {
         return $this->get();
     }
 }
--- tests/DisallowedCharacterTerminatedStringTest.php
+++ tests/DisallowedCharacterTerminatedStringTest.php
@@ -1,13 +1,37 @@
 <?php
 
-class DisallowedCharacterTerminatedStringTest extends PHPUnit_Framework_TestCase {
+declare(strict_types=1);
 
-    public function testStringIsTerminatedByDisallowedCharacter() {       
-        $string = new \webignition\DisallowedCharacterTerminatedString\DisallowedCharacterTerminatedString();
+namespace webignition\DisallowedCharacterTerminatedString\Tests;
+
+use webignition\DisallowedCharacterTerminatedString\DisallowedCharacterTerminatedString;
+
+class DisallowedCharacterTerminatedStringTest extends \PHPUnit\Framework\TestCase
+{
+    public function testCastingToString()
+    {
+        $string = new DisallowedCharacterTerminatedString();
+
+        $string->set('input value');
+        $this->assertEquals('input value', (string)$string);
+    }
+
+    public function testStringIsTerminatedByDisallowedCharacter()
+    {
+        $string = new DisallowedCharacterTerminatedString();
         $string->addDisallowedCharacterCode(ord(' '));
-        
+
         $string->set('will-be-terminated-by-first-space and-should-not-include-this');
         $this->assertEquals('will-be-terminated-by-first-space', $string->get());
     }
-    
+
+    public function testIgnoreEndOfLineComment()
+    {
+        $string = new DisallowedCharacterTerminatedString();
+        $string->addDisallowedCharacterCode(ord('#'));
+
+        $string->set('value #comment');
+        $this->assertEquals('value ', $string->get());
+    }
+
 }
